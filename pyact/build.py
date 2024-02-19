0db975fb75c03beeaed1d25de5106ef389eae768
franck403 <79372051+franck403@users.noreply.github.com>
2024-02-19 15:08:29 -0500

    fixing bugs

--- pyact/build.py
+++ pyact/build.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 class build:
     def __init__(self,filename):
@@ -9,3 +10,33 @@ class build:
         except:
             print('rebuilding')
         os.chdir('JSbuild/')
+    def builder(self):
+        directory = '/path/to/your/directory'
+        files_and_dirs = os.listdir(directory)
+        for file in files_and_dirs:
+            file_path = os.path.join(directory, file)
+            if os.path.isfile(file_path):
+                if file_path.endswith(".pyact") or file_path.endswith(".py"):
+                    print(file_path)
+                    self.buildFile(file_path)
+    def buildFile(self,path):
+        url = "https://api.extendsclass.com/convert/python/es6"
+        headers = {
+            "Content-Type": "text/plain;charset=UTF-8",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+        }
+        file = open('path','r').read()
+        data = {
+            "body": file
+        }
+
+        response = requests.post(url, headers=headers, data=data)
+
+        if response.ok:
+            data = response.json()
+            js =  data['stdout']
+        else:
+            print("Error fetching data:", response.status_code, response.reason)
+        open('')
