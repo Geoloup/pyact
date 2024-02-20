b7441436cff43fdb7b512e2f78b9869dde63c132
franck403 <79372051+franck403@users.noreply.github.com>
2024-02-20 13:55:10 -0500

    adding tinhg

--- pyact/__init__.py
+++ pyact/__init__.py
@@ -1,10 +1,3 @@
 # api for the html element for pycharm for exemple
-import inspect
-
-class element:     
-  def customElement(func):
-      def inner1(*args, **kwargs):
-          returned_value = func(*args, **kwargs)
-          return returned_value
-           
-      return inner1
+from pyact.api import *
+from pyact.build import *
